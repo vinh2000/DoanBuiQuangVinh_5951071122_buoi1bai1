014ef1ee6ccdfd93f518081a249b8b9c38b33516
vinh2000 <75476972+vinh2000@users.noreply.github.com>
2021-03-22 16:16:14 +0700

    Add project files.

---
+++ DEMOCRUD/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DEMOCRUD.Models
+{
+    public class Employee
+    {
+        public int Sr_no { get; set; } = 0;
+        public string Emp_name { get; set; } = "";
+        public string City { get; set; } = "";
+        public string State { get; set; } = "";
+        public string Country { get; set; } = "";
+        public string Department { get; set; } = "";
+        public string flag { get; set; } = "";
+    }
+}
